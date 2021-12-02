14684fd3eb2fd219ef2d53b3c4be2c5ef76506bd
Mazharul Islam <mazharulislam742@yahoo.com>
2021-12-02 14:52:52 +0600

    Updated

--- app/src/main/java/com/example/roomdatabse/fragment/update/UpdateFragment.kt
+++ app/src/main/java/com/example/roomdatabse/fragment/update/UpdateFragment.kt
@@ -1,17 +1,17 @@
 package com.example.roomdatabse.fragment.update
 
+import android.app.AlertDialog
 import android.icu.number.IntegerWidth
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextUtils
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.sqlite.db.SupportSQLiteOpenHelper.Configuration.builder
 import com.example.roomdatabse.R
 import com.example.roomdatabse.model.User
 import com.example.roomdatabse.viewmodel.UserViewModel
@@ -19,6 +19,8 @@ import kotlinx.android.synthetic.main.fragment_add.*
 import kotlinx.android.synthetic.main.fragment_update.*
 import kotlinx.android.synthetic.main.fragment_update.view.*
 import org.w3c.dom.Text
+import java.util.stream.DoubleStream.builder
+import java.util.stream.IntStream.builder
 
 class UpdateFragment : Fragment() {
 
@@ -33,25 +35,27 @@ class UpdateFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_update, container, false)
 
-        mUserViewModel  = ViewModelProvider(this).get(UserViewModel::class.java)
+        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
 
         view.etUpdFirstName.setText(args.currentUser.firstName)
         view.etUpdLastName.setText(args.currentUser.lastName)
         view.etUpdAge.setText(args.currentUser.age.toString())
 
-        view.btnUpdAdd.setOnClickListener{
+        view.btnUpdAdd.setOnClickListener {
             UpdateItem()
         }
+
+        setHasOptionsMenu(true)
 
         return view
     }
 
-    private fun UpdateItem(){
+    private fun UpdateItem() {
         val firstName = etUpdFirstName.text.toString()
         val lastName = etUpdLastName.text.toString()
         val age = Integer.parseInt(etUpdAge.text.toString())
 
-        if(inputCheck(firstName, lastName, etUpdAge.text)){
+        if (inputCheck(firstName, lastName, etUpdAge.text)) {
             val updatedUser = User(args.currentUser.id, firstName, lastName, age)
             mUserViewModel.updateUser(updatedUser)
 
@@ -59,15 +63,43 @@ class UpdateFragment : Fragment() {
 
             findNavController().navigate(R.id.action_updateFragment_to_listFragment)
 
-        }else{
-            Toast.makeText(requireContext(), "Please Fill all the fields", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(requireContext(), "Please Fill all the fields", Toast.LENGTH_SHORT)
+                .show()
 
         }
 
     }
 
-    private fun inputCheck(firstName: String, lastName: String, age: Editable): Boolean{
+    private fun inputCheck(firstName: String, lastName: String, age: Editable): Boolean {
         return !(TextUtils.isEmpty(firstName) && TextUtils.isEmpty(lastName) && age.isEmpty())
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.delete_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_delete) {
+            deleteUser()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun deleteUser() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes") { _, _ ->
+            mUserViewModel.deleteUser(args.currentUser)
+            Toast.makeText(requireContext(), "Successfully removed ${args.currentUser.firstName}", Toast.LENGTH_SHORT)
+                .show()
+            findNavController().navigate(R.id.action_updateFragment_to_listFragment)
+        }
+        builder.setNegativeButton("No") { _, _ ->
+
+        }
+
+        builder.setTitle("Delete ${args.currentUser.firstName} ?")
+        builder.setMessage("Are you sure want to delete ${args.currentUser.firstName} ?")
+        builder.create().show()
+    }
 }
--- app/src/main/java/com/example/roomdatabse/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/roomdatabse/viewmodel/UserViewModel.kt
@@ -10,24 +10,38 @@ import com.example.roomdatabse.model.User
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class UserViewModel(application: Application): AndroidViewModel(application) {
+class UserViewModel(application: Application) : AndroidViewModel(application) {
     val readAllData: LiveData<List<User>>
     private val repository: UserRepository
 
-    init{
+    init {
         val userDao = UserDatabase.getDatabase(application).userDao()
         repository = UserRepository(userDao)
         readAllData = repository.readAllData
     }
 
-    fun addUser(user: User){
-        viewModelScope.launch(Dispatchers.IO){
+    fun addUser(user: User) {
+        viewModelScope.launch(Dispatchers.IO) {
             repository.addUser(user)
         }
     }
-    fun updateUser(user: User){
-        viewModelScope.launch (Dispatchers.IO){
+
+    fun updateUser(user: User) {
+        viewModelScope.launch(Dispatchers.IO) {
             repository.updateUser(user)
         }
     }
+
+    fun deleteUser(user: User) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.deleteUser(user)
+        }
+    }
+
+    fun deleteAllUser() {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.deleteAllUser()
+        }
+    }
+
 }
